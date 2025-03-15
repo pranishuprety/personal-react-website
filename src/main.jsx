112e65a6484953b6563dc2aebcdb2abe6124d6c9
Pranish Uprety <71381948+pranishuprety@users.noreply.github.com>
2025-03-14 21:15:08 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './App.css';
-import App from './index.jsx';
+import './index.css';
+import App from './App.jsx';
 import { ThemeProvider } from './common/ThemeContext.jsx';
 
 createRoot(document.getElementById('root')).render(
