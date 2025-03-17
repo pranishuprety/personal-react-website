ebbc618b5ac9d0148c9a1ee8c3a5d1df2e9a05be
Pranish Uprety <pranishup642@gmail.com>
2025-03-17 15:30:01 -0400

    changed footer

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from './FooterStyles.module.css'
 function Footer() {
   return (
     <section id='footer' className={styles.container}>
-        <p>&copy;2025 Pranish Uprety<br/> All right reserved.</p>
+        <p>&copy;2025 Pranish Uprety<br/> All rights reserved.</p>
     </section>
   )
 }
