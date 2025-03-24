81188e6a180cb5004dda5227e674e03bbeaccc7e
Pranish Uprety <pranishup642@gmail.com>
2025-03-24 17:04:15 -0400

    api key changes in forms

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
 		<section id="contact" className={styles.container}>
 			<h1 className="sectionTitle">Contact</h1>
 			<form name="contact" action="https://api.web3forms.com/submit" method="POST">
-				<input type="hidden" name="access_key" value="99c93ad44-c8f9-4c25-83a1-b174a762b0fb" />
+				<input type="hidden" name="access_key" value="9c93ad44-c8f9-4c25-83a1-b174a762b0fb" />
 				<div className="formGroup">
 					<label htmlFor="name" hidden>
 						Name
