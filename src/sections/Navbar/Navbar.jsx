98c7144b9d7314044274873f41bd6049614c4222
Pranish Uprety <pranishup642@gmail.com>
2025-03-29 01:05:04 -0400

    navbar test

---
+++ src/sections/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul className={styles.navList}>
+        <li className={styles.navItem}>
+          <a href="/" className={styles.navLink}>Home</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#projects" className={styles.navLink}>Projects</a>
+        </li>
+        <li className={styles.navItem}>
+          <a href="#contact" className={styles.navLink}>Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
